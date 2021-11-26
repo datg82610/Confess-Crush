4f8bc59b13fd4940fc5ba0adcee99c94eb668bd6
datg82610 <95065432+datg82610@users.noreply.github.com>
2021-11-26 12:33:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Tán Quỳnh",
+    introTitle: 'Quỳnh Quỳnh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -13,8 +13,8 @@ const CONFIG = {
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'Tui biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 9h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://github.com/zukahai/Confess-Crush' //https://l.facebook.com/l.php?u=https%3A%2F%2Fm.me%2Fdatg.huynh.7%3Ffbclid%3DIwAR19WYW1EI5tDMvNdxItSTctbhKCZPFTYqvjunvEoyMKPGn3LOCxn4A3pMA&h=AT2iEf5WHT8Rdt6-ivAaX8Ix6obi2tL86sQhTkYFNVUZrNISBDpsDPjGAh9bMKILnOD8afDpTQ25loX_Ef5IvNDxWdDY9TqSRevgwagtYgFeGsfprCVby2Yp6zjsgjrQ-mxPmg
 }
